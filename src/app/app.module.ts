c3761ab4c5be208e3fb7562de1059088cdfd7d94
Sugavanesh <saisunilapps@gmail.com>
2024-10-07 15:00:19 +0530

    added landing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { MainComponent } from './main/main.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    MainComponent
+    MainComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
